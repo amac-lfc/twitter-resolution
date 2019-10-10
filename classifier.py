b140bbd908beb6f6bc6936433770b64b3f35f243
lgiurini <40581437+lgiurini@users.noreply.github.com>
2019-10-10 12:52:14 -0500

    10/10 update

--- classifier.py
+++ classifier.py
@@ -9,7 +9,7 @@ from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 import warnings
 from sklearn.linear_model import LogisticRegression
-# from yellowbrick.classifier import ConfusionMatrix
+from yellowbrick.classifier import ConfusionMatrix
 from collections import Counter
 from sklearn.datasets import make_classification
 from imblearn.over_sampling import SMOTE
@@ -55,25 +55,36 @@ print(f"Accuracy score for Random Forest Classifier is: {accuracy_score(Ytest, y
 
 
 tr2 = LogisticRegression()
-# tr2.fit(Xtrain, Ytrain)
+tr2.fit(Xtrain, Ytrain)
 # print("importances =", tr2.feature_importances_)
 
-# y_predict = tr2.predict(Xtest)
-# print(f"Accuracy score for Logistic Regression Classifier is: {accuracy_score(Ytest, y_predict)}")
-# print("importances=",tr2.coef_)
+y_predict = tr2.predict(Xtest)
+print(f"Accuracy score for Logistic Regression Classifier is: {accuracy_score(Ytest, y_predict)}")
+print("importances=",tr2.coef_)
 
 #confusion ConfusionMatrix only commented bc lab doesnt have yellow brick
-# cm = ConfusionMatrix(tr2, classes=[0,1])
-# cm.fit(Xtrain, Ytrain)
-# cm.score(Xtest, Ytest)
-# cm.show()
+cm = ConfusionMatrix(tr2, classes=[0,1])
+cm.fit(Xtrain, Ytrain)
+cm.score(Xtest, Ytest)
+cm.show()
 
 # SMOTE
 
-c1, c2 = make_classification(n_classes=2, class_sep=2, weights=[0.1, 0.9],
-                             n_informative=3, n_redundant=1, flip_y=0,
-                             n_features=20, n_clusters_per_class=1, n_samples=1000, random_state=10)
-print('Original dataset shape %s' % Counter(c2))
-sm = SMOTE(random_state=42)
-c1_res, c2_res = sm.fit_resample(c1, c2)
-print('Resampled dataset shape %s' % Counter(c2_res))
+
+
+print("before =", np.bincount(Ytrain))
+smt = SMOTE()
+Xtrain, Ytrain = smt.fit_sample(Xtrain, Ytrain)
+print("after =", np.bincount(Ytrain))
+
+print(Xtrain, Ytrain)
+ac = accuracy_score(Ytrain, y_predict)
+print(ac)
+
+
+# c1, c2 = make_classification(n_classes=2, class_sep=2, weights=[0.1, 0.9],
+#                              n_informative=3, n_redundant=1, flip_y=0,
+#                              n_features=20, n_clusters_per_class=1, n_samples=1000, random_state=10)
+# print('Original dataset shape %s' % Counter(c2))
+# c1_res, c2_res = smt.fit_resample(c1, c2)
+# print('Resampled dataset shape %s' % Counter(c2_res))
